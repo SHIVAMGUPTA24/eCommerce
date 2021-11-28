ebf9d37d714bb01702abc54a0a160ee85d5f6722
SHIVAMGUPTA24 <shivam20292414@gmail.com>
2021-11-28 09:14:30 +0530

    Repository Layer and Entity creation and Service for add and delete

---
+++ product/src/main/java/com/retailstore/product/service/ProductService.java
@@ -0,0 +1,13 @@
+package com.retailstore.product.service;
+
+import com.retailstore.product.entity.Product;
+import org.springframework.http.ResponseEntity;
+
+public interface ProductService {
+
+    ResponseEntity<?> addProduct(Product product);
+    ResponseEntity<?> deleteProduct(long productId);
+    ResponseEntity<?> updateProduct();
+    ResponseEntity<?> searchProduct();
+
+}
