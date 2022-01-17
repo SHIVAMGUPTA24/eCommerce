d3c5fca98278f183ce4c75138a05d1f5ba80ba8d
Shivam Gupta <shivamgupta@Shivams-MacBook-Air.local>
2022-01-17 22:22:25 +0530

    Order MS layers

--- order/src/main/java/com/retailstore/order/service/OrderService.java
+++ order/src/main/java/com/retailstore/order/service/OrderService.java
@@ -4,5 +4,11 @@ import com.retailstore.order.entity.Order;
 import org.springframework.http.ResponseEntity;
 
 public interface OrderService {
-    ResponseEntity<Order> getOrderById(int id);
+    ResponseEntity<?> getOrderById(int id);
+
+    ResponseEntity<?> createOrder(Order order);
+
+    ResponseEntity<?> updateOrder(int id, Order order);
+
+    ResponseEntity<?> deleteOrder(int id);
 }
